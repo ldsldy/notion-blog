6ab882f8b76c7f6f22f584000f84175f7da2effa
LDY <ldsldy@naver.com>
2026-08-11 11:03:59 +0900

    fix: fill table width and center image viewer

--- components/post-layout.tsx
+++ components/post-layout.tsx
@@ -88,7 +88,7 @@ export default function PostLayout({
         </nav>
       </aside>
 
-      <div className="post-detail relative left-1/2 w-[min(calc(100vw-2rem),80rem)] -translate-x-1/2">
+      <div className="post-detail relative w-[min(calc(100vw-2rem),80rem)] self-center">
         {children}
       </div>
     </>
